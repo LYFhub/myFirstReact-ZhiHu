a1d79b83d3f6c6bb6774c170ddec748ea5ec8f94
LYFhub <liyf963@163.com>
2016-12-16 17:07:41 +0800

    add changed files

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 var config = {
-	entry: './main.js', // 指定打包的入口文件
+	entry: './todolist/main.js', // 指定打包的入口文件
 
 	output: {           // 配置打包结果，path 定义了输出的文件夹，filename 定义打包结果文件的名称
-		path: './',
-		filename: 'index.js'
+		path: './todolist',
+		filename: 'todolist.js'
 	},
 
 	devServer: {        // 设置服务器端口号为7777， 端口号可以自己设定
@@ -11,7 +11,8 @@ var config = {
 		port: 7777
 	},
 
-	module: {           // 定义了对模块的处理逻辑，这里可以用loaders定义了一系列的加载器，以及一些正则。当需要加载的文件匹配test的正则时，就会调用后面的loader对文件进行处理，这正是webpack强大的原因。
+	module: {           // 定义了对模块的处理逻辑，这里可以用loaders定义了一系列的加载器，以及一些正则。
+		//当需要加载的文件匹配test的正则时，就会调用后面的loader对文件进行处理，这正是webpack强大的原因。
 		loaders: [{
 			test:/\.jsx?$/,
 			exclude: /node_modules/,
